dad2cadc3aa52987eb695b2c690c778bd3bbfd0c
cloepark03-ui <cloepark03@gmail.com>
2025-10-25 10:54:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "이 앱은 OpenAI의 GPT-3.5 모델을 활용한 간단한 챗봇입니다. "
+    "이 앱을 사용하려면 OpenAI API 키가 필요하며, [여기](https://platform.openai.com/account/api-keys)에서 발급받을 수 있습니다. "
+    "또한, 이 앱을 단계별로 직접 만들어보고 싶다면 [튜토리얼](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)을 참고하세요."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
